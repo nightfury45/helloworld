9a4826aa25e9d7151bea213eb5ebef9bc6b03278
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-03 17:04:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 </head>
 <body>
 <?php echo '<p>This is website after edited</p>'; ?>
+$conn=mysql_connect("ec2-54-146-91-153.compute-1.amazonaws.com","viurkgajlugnms","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39");
+mysql_select_db("d8hdjv4le2jqun",$conn);
 </body>
 </html>
 		
