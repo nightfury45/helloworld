8bc890f6cef8aca09c14e9fcf424ff2fec265be0
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-03 17:07:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <title>PHP Test</title>
 </head>
 <body>
-<?php echo '<p>This is website after edited</p>'; ?>
 <?php $conn=mysql_connect("ec2-54-146-91-153.compute-1.amazonaws.com","viurkgajlugnms","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39");
+	echo 'connected';
 mysql_select_db("d8hdjv4le2jqun",$conn);?>
 </body>
 </html>
