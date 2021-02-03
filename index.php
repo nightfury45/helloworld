196c426ad966c801e406078e45ea4caa9f5a5576
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-03 17:36:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,44 +4,12 @@
 </head>
 <body>
 <?php
-$host    = "ec2-54-146-91-153.compute-1.amazonaws.com";
-$user    = "viurkgajlugnms";
-$pass    = "3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39";
-$db_name = "d8hdjv4le2jqun";
-
-//create connection
-$connection = mysqli_connect($host, $user, $pass, $db_name);
-
-//test if connection failed
-if(mysqli_connect_errno()){
-    die("connection failed: "
-        . mysqli_connect_error()
-        . " (" . mysqli_connect_errno()
-        . ")");
+try{
+	$myPDO = new PDO("pgsql:host=ec2-54-146-91-153.compute-1.amazonaws.com,dbname=d8hdjv4le2jqun","viurkgajlugnms","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39");
+	echo "Connected to PostgreSQL with PDO";
+}catch(PDOException $e){
+	echo $e->getMessage();
 }
-
-//get results from database
-$result = mysqli_query($connection,"SELECT * FROM test_lab6");
-$all_property = array();  //declare an array for saving property
-
-//showing property
-echo '<table class="data-table">
-        <tr class="data-heading">';  //initialize table tag
-while ($property = mysqli_fetch_field($result)) {
-    echo '<td>' . $property->name . '</td>';  //get field name for header
-    array_push($all_property, $property->name);  //save those to array
-}
-echo '</tr>'; //end tr tag
-
-//showing all data
-while ($row = mysqli_fetch_array($result)) {
-    echo "<tr>";
-    foreach ($all_property as $item) {
-        echo '<td>' . $row[$item] . '</td>'; //get items using property value
-    }
-    echo '</tr>';
-}
-echo "</table>";
 ?>
 
 
