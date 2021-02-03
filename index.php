a7587e627c768e8cf407d43d5150a9d5908a6b8b
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-03 17:27:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,32 +4,47 @@
 </head>
 <body>
 <?php
-$con=mysqli_connect("ec2-54-146-91-153.compute-1.amazonaws.com","viurkgajlugnms","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39","d8hdjv4le2jqun");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+$host    = "ec2-54-146-91-153.compute-1.amazonaws.com";
+$user    = "viurkgajlugnms";
+$pass    = "3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39";
+$db_name = "d8hdjv4le2jqun";
+
+//create connection
+$connection = mysqli_connect($host, $user, $pass, $db_name);
+
+//test if connection failed
+if(mysqli_connect_errno()){
+    die("connection failed: "
+        . mysqli_connect_error()
+        . " (" . mysqli_connect_errno()
+        . ")");
 }
 
-$result = mysqli_query($con,"SELECT * FROM test_lab6");
+//get results from database
+$result = mysqli_query($connection,"SELECT * FROM test_lab6");
+$all_property = array();  //declare an array for saving property
 
-echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>";
+//showing property
+echo '<table class="data-table">
+        <tr class="data-heading">';  //initialize table tag
+while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    array_push($all_property, $property->name);  //save those to array
+}
+echo '</tr>'; //end tr tag
 
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['FirstName'] . "</td>";
-echo "<td>" . $row['LastName'] . "</td>";
-echo "</tr>";
+//showing all data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '</td>'; //get items using property value
+    }
+    echo '</tr>';
 }
 echo "</table>";
-
-mysqli_close($con);
 ?>
+
+
 
 </body>
 </html>
