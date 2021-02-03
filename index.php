8b8165e282a1dd7e8b460bb12d9da7aedfe36154
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-03 17:41:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 try{
-	$myPDO = new PDO("pgsql:host=ec2-54-146-91-153.compute-1.amazonaws.com,dbname=d8hdjv4le2jqun","d8hdjv4le2jqun","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39");
+	$myPDO = new PDO("pgsql:host=ec2-54-146-91-153.compute-1.amazonaws.com,dbname=d8hdjv4le2jqun","viurkgajlugnms","3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39");
 	echo "Connected to PostgreSQL with PDO";
 }catch(PDOException $e){
 	echo $e->getMessage();
