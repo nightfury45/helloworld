263fe5336f1d810b7cf8fbfc720788c164dd44aa
nightfury45 <51909158+nightfury45@users.noreply.github.com>
2021-02-05 13:52:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 		<title>PHP Test</title>
 	</head>
 	<body>
-		<?php
-			echo '<p>TEST DATABASE </p>';
-			# Heroku credential
+		<?php 
+			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
+			# Heroku credential 
 			$host_heroku = "ec2-54-146-91-153.compute-1.amazonaws.com";
 			$db_heroku = "d8hdjv4le2jqun";
 			$user_heroku = "viurkgajlugnms";
-			$pw_heroku = "3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39";
+			$pw_heroku = "    3d33c822e23329eb406e2659a016cee4759ac092f4f96157f4a8ba65a3dcbd39";
 			# Create connection to Heroku Postgres
 			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
 			$pg_heroku = pg_connect($conn_string);
+			
 			if (!$pg_heroku)
 			{
 				die('Error: Could not connect: ' . pg_last_error());
 			}
-			#Get data by query
+			# Get data by query
 			$query = 'select * from test_lab6';
 			$result = pg_query($pg_heroku, $query);
 			# Display data column by column
@@ -29,10 +30,10 @@
 				echo '<td>' . $fieldName . '</td>';
 				$i = $i + 1;
 			}
-			echo '</tr>'
+			echo '</tr>';
 			# Display data row by row
 			$i = 0;
-			while ($row = pg_fetch_row($result))
+			while ($row = pg_fetch_row($result)) 
 			{
 				echo '<tr>';
 				$count = count($row);
@@ -48,7 +49,9 @@
 				$i = $i + 1;
 			}
 			pg_free_result($result);
+
 			echo '</table></body></html>';
-		?>
+
+		?> 
 	</body>
 </html>
